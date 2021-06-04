0ef501942b38626824f8f294dd5a0b31bb5e7d26
Luke Johnston <luketjohnston32@gmail.com>
2021-06-04 00:05:09 -0700

    updated my cuda install, now dont need the lines at the beginning to set memory growth on device

--- train.py
+++ train.py
@@ -19,11 +19,6 @@ from visualize import saveImage
 
 QUICK_TEST = False
 
-if tf.config.list_physical_devices('GPU'):
-  # For some reason this is necessary to prevent error
-  physical_devices = tf.config.experimental.list_physical_devices('GPU')
-  tf.config.experimental.set_memory_growth(physical_devices[0], True)
-
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import numpy as np
@@ -31,7 +26,7 @@ import numpy as np
 import wgan
 
 RUN_EAGER = False
-PROFILE = False
+PROFILE = True
 
 if RUN_EAGER:
   tf.config.run_functions_eagerly(True)
@@ -98,23 +93,22 @@ if __name__ == '__main__':
   for epoch in range(EPOCHS):
     print("EPOCH %d/%d" % (epoch, EPOCHS))
     
-
-    if epoch == 2 and PROFILE:
-      tf.profiler.experimental.start('logdir')
-    if epoch == 3 and PROFILE:
-      tf.profiler.experimental.stop()
-
     for i,real_images in enumerate(ds):
+      if epoch == 0 and i == 10 and PROFILE:
+        tf.profiler.experimental.start('logdir')
+      if epoch == 0 and i == 20 and PROFILE:
+        tf.profiler.experimental.stop()
+        
 
-        critic_loss = gan.trainCritic(real_images, real_images.shape[0])
-        gan_save['critic_losses'] += [critic_loss]
+      critic_loss = gan.trainCritic(real_images, real_images.shape[0])
+      gan_save['critic_losses'] += [critic_loss]
 
-        # train only the critic for the first INITIAL_CRITIC_EPOCHS
-        if (not i % NUM_CRITIC_PER_GEN) and (epoch >= INITIAL_CRITIC_EPOCHS):
-          gen_loss = gan.trainGen(BATCH_SIZE)
-          gan_save['gen_losses'] += [gen_loss]
-          loss_str = ''.join('{:6f}, '.format(lossv) for lossv in (critic_loss, gen_loss))
-          print('%d/%d: critic, gen: %s' % (i,ds.cardinality(),loss_str))
+      # train only the critic for the first INITIAL_CRITIC_EPOCHS
+      if (not i % NUM_CRITIC_PER_GEN) and (epoch >= INITIAL_CRITIC_EPOCHS):
+        gen_loss = gan.trainGen(BATCH_SIZE)
+        gan_save['gen_losses'] += [gen_loss]
+        loss_str = ''.join('{:6f}, '.format(lossv) for lossv in (critic_loss, gen_loss))
+        print('%d/%d: critic, gen: %s' % (i,ds.cardinality(),loss_str))
        
     print('Saving model...')
 
